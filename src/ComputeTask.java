c78cb0bf7f753964ac4e4131f5fe6dfac0d174b7
kamurashev <kamurashev@gmail.com>
2021-03-28 21:24:57 +0300

    initial

---
+++ src/ComputeTask.java
@@ -0,0 +1,56 @@
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class ComputeTask {
+    private static final int START_NUMBER = 1;
+    private static final int END_NUMBER =
+            System.getenv("startNumber") == null ? 10000 : Integer.parseInt(System.getenv("startNumber"));
+    private static final LocalDateTime START_TIME = LocalDateTime.now();
+
+    private List<Integer> primes;
+    private Duration completionTime;
+
+    public ComputeTask() {
+        findPrimes();
+    }
+
+    private void findPrimes() {
+        primes =
+        IntStream.rangeClosed(START_NUMBER, END_NUMBER)
+                 .boxed()
+                 .filter(this::isPrime)
+                 .toList();
+        completionTime = Duration.between(START_TIME, LocalDateTime.now());
+    }
+
+    private boolean isPrime(int current) {
+        return IntStream.range(2, current)
+                 .noneMatch(div -> current % div == 0);
+    }
+
+    public int getProcessedNumbers() {
+        return END_NUMBER;
+    }
+
+    public List<Integer> getPrimes() {
+        return primes;
+    }
+
+    public int getPrimesCount() {
+        return primes.size();
+    }
+
+    public long getCompletionTimeMillis() {
+        return completionTime.toMillis();
+    }
+
+    public static void main(String[] args) {
+        ComputeTask computeTask = new ComputeTask();
+        System.out.println(
+                "Processed " + computeTask.getProcessedNumbers() +
+                        " numbers, found " + computeTask.getPrimesCount() +
+                        " prime numbers, completion time " + computeTask.getCompletionTimeMillis() / 1000.0 + " s");
+    }
+}
