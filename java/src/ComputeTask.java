51116a7681e4b61caf4b025a45dca4a3cfecc063
Kirill Murashev <k.a.murashev@gmail.com>
2021-04-05 21:21:44 +0300

    - java parallel stream approach;

--- java/src/ComputeTask.java
+++ java/src/ComputeTask.java
@@ -1,74 +1,33 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
- * commented out lambda style code is taking approx. 50-100% more time to execute, though it looks nicer,
- * e.g. 1.5...2.1s vs 1.06...1.2s on my Dell latitude 5590 i7 8750u
- * so when you write your code you can choose either performance or younger look huh?)
- *
+ * For parallel execution best performance is achieved using parallel stream
+ * it is better then for loop + CompletableFeature for a negligible amount (about 0.02sec)
+ * but look at that short beautiful code...
+ * 0.31-0.4sec for 100 000 range on my Dell latitude 5590 i7 8750u
  * @author Kirill Murashev <krill.murashev@gmail.com>
  */
 public class ComputeTask {
     private static final long START_TIME = System.currentTimeMillis();
-    private static final int THREADS = System.getenv("threads") == null ?
-            Runtime.getRuntime().availableProcessors() : Integer.parseInt(System.getenv("threads"));
     private static final int START_NUMBER = 1;
     private static final int END_NUMBER = System.getenv("endNumber") == null ?
-            10000 : Integer.parseInt(System.getenv("endNumber"));
-    private static final int RANGE = END_NUMBER / THREADS;
-
-    private static class AsyncComputeTask implements Supplier<List<Integer>> {
-        private final int startNumber;
-        private final int endNumber;
-
-        public AsyncComputeTask(int startNumber, int endNumber) {
-            this.startNumber = startNumber;
-            this.endNumber = endNumber;
-        }
-
-        @Override
-        public List<Integer> get() {
-            final List<Integer> result = new ArrayList<>(endNumber);
-            for (int number = startNumber; number <= endNumber; number++) {
-                if (isPrime(number)) {
-                    result.add(number);
-                }
-            }
-            System.out.printf("Worker id: %s delivered result of %d, time spent %f s%n",
-                              Thread.currentThread().getId(), result.size(), getExecutionTime());
-            return result;
-        }
-
-        private boolean isPrime(int number) {
-            for (int div = 2; div < number; div++) {
-                if (number % div == 0) {
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
+            100000 : Integer.parseInt(System.getenv("endNumber"));
 
     public static void main(String[] args) {
-        ArrayList<CompletableFuture<List<Integer>>> asyncTasks = new ArrayList<>(THREADS);
-        for (int i = 1; i <= THREADS; i++) {
-            int endNumber = i == THREADS ? END_NUMBER : RANGE * i;
-            int startFrom = RANGE * (i - 1) + START_NUMBER;
-            asyncTasks.add(CompletableFuture.supplyAsync(new AsyncComputeTask(startFrom, endNumber)));
-        }
-
-        List<Integer> primes = new ArrayList<>(END_NUMBER);
-        for (CompletableFuture<List<Integer>> asyncTask : asyncTasks) {
-            primes.addAll(asyncTask.join());
-        }
+        var primes = IntStream.rangeClosed(START_NUMBER, END_NUMBER).boxed()
+                              .parallel().filter(ComputeTask::isPrime).collect(Collectors.toList());
 
         System.out.printf("Processed %d numbers, found %d prime numbers, completion time %f s%n",
-                          END_NUMBER, primes.size(), getExecutionTime());
+                          END_NUMBER, primes.size(), (System.currentTimeMillis() - START_TIME) / 1000.0);
     }
 
-    private static Double getExecutionTime() {
-        return  (System.currentTimeMillis() - START_TIME) / 1000.0;
+    private static boolean isPrime(int number) {
+        for (int div = 2; div < number; div++) {
+            if (number % div == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
