2a8e36dc6b1fe3c657ba74dd1d294b5ef87c0863
kamurashev <kamurashev@gmail.com>
2023-04-28 05:24:35 -0500

    Added omp.h based multithreaded solution

--- c/build.sh
+++ c/build.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
+#brew install llvm libomp
+#export LDFLAGS="-L/opt/homebrew/opt/libomp/lib"
+#export CPPFLAGS="-I/opt/homebrew/opt/libomp/include"
+#xport LDFLAGS="-L/opt/homebrew/opt/llvm/lib"
+#export CPPFLAGS="-I/opt/homebrew/opt/llvm/include"
+#export CC=/usr/local/opt/llvm/bin/clang
 
 echo "removing previous executable..."
 rm computeTask
-gcc -Wall -O3 computeTask.c -o computeTask
+#gcc -I/opt/homebrew/opt/libomp/include -Wall -O3 computeTask.c -fopenmp -o computeTask
+/opt/homebrew/opt/llvm/bin/clang -Wall -O3 computeTask.c -fopenmp -o computeTask
 echo "built as ./computeTask"
 echo "Done, happy hacking!"
