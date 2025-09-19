999bd0b52bd522ddf7d24e5d6afc65268d5fe019
kamurashev <k.a.murashev@gmail.com>
2025-09-19 16:52:05 -0500

    added java ver arg for graal build script

--- java/benchmark.sh
+++ java/benchmark.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-source ./build-native-graal-runtime.sh
-source ./make-runtime.sh
+#source ./build-native-graal-runtime.sh
+#source ./make-runtime.sh
 
-hyperfine -r 100 --warmup 10 ./jre-image/bin/launcher
+hyperfine -r 35 --warmup 10 ./jre-image/bin/launcher
 sleep 2
-hyperfine -r 100 --warmup 10 "java --module-path ./build --module compute_task/compute_task.ComputeTask"
+hyperfine -r 35 --warmup 10 "java --module-path ./build --module compute_task/compute_task.ComputeTask"
 sleep 2
-hyperfine -r 100 --warmup 10 ./native-image/computetask
+hyperfine -r 45 --warmup 10 ./native-image/computetask
